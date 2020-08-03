9013e02eee977ae359276dffe5e10fca09df60ca
mamulini <mamulini@yahoo.com>
2020-08-03 19:58:03 +0300

    Updates

---
+++ precache-manifest.2e1bac1169b02fbf63e6d6e4a5b5f2b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5d501484ae8c58fe0092ce1c08fded",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "c243767dae4434d881da",
+    "url": "/monsters-relodex/static/css/main.3e09f2d9.chunk.css"
+  },
+  {
+    "revision": "d2644eed02d7ddd0a69b",
+    "url": "/monsters-relodex/static/js/2.1b013cc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.1b013cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c243767dae4434d881da",
+    "url": "/monsters-relodex/static/js/main.61a8288f.chunk.js"
+  },
+  {
+    "revision": "8136ad99f44bb053ab2c",
+    "url": "/monsters-relodex/static/js/runtime-main.1797798b.js"
+  }
+]);
